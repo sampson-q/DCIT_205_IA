ab71e8fda73650845140060bdd6073a1f6854c27
sampson-q <quarmysokpoli@gmail.com>
2023-11-22 03:14:33 +0000

    video background

---
+++ src/components/VideoBackground.js
@@ -0,0 +1,23 @@
+// VideoBackground.js
+
+import React from "react";
+import "../styles/VideoBackground.css"; // Import your CSS file
+
+function VideoBackground() {
+    return (
+        <div className="video-background">
+            
+            <video autoPlay loop muted>
+                <source src="https://dcs.ug.edu.gh/video-bg.mp4" type="video/mp4" />
+            </video>
+
+            <div className="overlay"></div>
+
+            <div className="text-overlay">
+                <h1>YOUR GATEWAY TO WORLD CLASS COMPUTER SCIENCE EDUCATION</h1>
+            </div>
+        </div>
+    );
+}
+
+export default VideoBackground;
